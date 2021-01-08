d1eba1afda6b86250e43b4bcd2995e2d8a51e825
Andrei Varapayeu <thisavoropaev@gmail.com>
2021-01-08 15:33:46 +0300

    update pdf size

--- pdf.js
+++ pdf.js
@@ -3,7 +3,7 @@ const htmlPdf = require('html-pdf');
 const htmlSourceFile = fs.readFileSync('index.html', 'utf8');
 
 htmlPdf
-  .create(htmlSourceFile, {width: '1320px', height: '1780px'})
+  .create(htmlSourceFile, {width: '980px', height: '1800px'})
   .toFile('andrei-varapayeu-cv.pdf', function(err, res) {
     if (err) return console.log(err);
     console.log(`PDF was created successfully: ${res.filename}`);
